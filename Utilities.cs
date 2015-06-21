0df8067788e5e1e03caa02f721a32bdcd1d8cd26
Corstiaan <corstiaanhesselink@gmail.com>
2015-06-21 20:46:44 +0200

    TTC

---
+++ Utilities.cs
@@ -0,0 +1,22 @@
+﻿using UIKit;
+
+namespace Fluxmatix.Mobile.iOS
+{
+	public static class Utilities
+	{
+		private static int _numberOfCallsToSetNetworkIndicatorVisible = 0;
+
+		public static void SetNetworkActivityIndicatorVisibility(bool setVisible) {
+			if (setVisible) {
+				_numberOfCallsToSetNetworkIndicatorVisible++;
+			}
+			else {
+				if(_numberOfCallsToSetNetworkIndicatorVisible != 0) {
+					_numberOfCallsToSetNetworkIndicatorVisible--;
+				}
+			}
+			UIApplication.SharedApplication.NetworkActivityIndicatorVisible = _numberOfCallsToSetNetworkIndicatorVisible > 0;
+		}
+	}
+}
+
